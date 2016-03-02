7af2751575ef504c8fd2ae2d0d070db4a1479bfd
gobaldia <gobaldia@gmail.com>
2016-03-01 21:30:22 -0300

    Update funciones.js

--- Obligatorio Taller 1/js/funciones.js
+++ Obligatorio Taller 1/js/funciones.js
@@ -1,7 +1,8 @@
-﻿window.onload = function() {	
+window.onload = function() {	
 	
 	document.getElementById("idErrorAñosExp").onblur = validarAñosExp();
 	cargarArrayEdades();
+	cargarArrayRangoEdades();
 	
 }
 
@@ -31,6 +32,28 @@ function cargarArrayEdades(){
 			options.value = i;			
 			options.text = i;	
 			comboEdades.appendChild(options);			
+		}
+	}
+}
+
+function cargarArrayRangoEdades() {
+	var comboRangoEdades = document.getElementById("idRangoEdad");
+
+	for (var i = 15; i < 100; i+=5) {
+		var options = document.createElement("option");
+
+		if (i == 15) {
+			options.value = "";
+			options.text = "Seleccione un rango de edad...";
+			comboRangoEdades.appendChild(options);
+		} else if (i == 20) {
+			options.value = "Menor a 20";
+			options.text = "Menor a 20";
+			comboRangoEdades.appendChild(options);
+		} else {
+			options.value = i-5 + " a " + i;
+			options.text = i-5 + " a " + i;
+			comboRangoEdades.appendChild(options);
 		}
 	}
 }
